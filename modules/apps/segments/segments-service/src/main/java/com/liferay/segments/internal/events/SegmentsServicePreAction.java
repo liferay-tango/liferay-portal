f417c8b81f3c3dfddb57eda7a8cf65f35c7d4012
Cristina González Castellano <cristina.gonzalez@liferay.com>
2020-10-10 15:51:56 +0200

    LPS-122025 Store user segments in http request (this way we will avoid recalculate segments in SegmentsEntryRoleContributor and SegmentsServicePreAction)

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/events/SegmentsServicePreAction.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/events/SegmentsServicePreAction.java
@@ -118,12 +118,17 @@ public class SegmentsServicePreAction extends Action {
 			return;
 		}
 
-		long[] segmentsEntryIds = _getSegmentsEntryIds(
-			httpServletRequest, themeDisplay.getScopeGroupId(),
-			themeDisplay.getUserId());
+		long[] segmentsEntryIds = (long[])httpServletRequest.getAttribute(
+			SegmentsWebKeys.SEGMENTS_ENTRY_IDS);
 
-		httpServletRequest.setAttribute(
-			SegmentsWebKeys.SEGMENTS_ENTRY_IDS, segmentsEntryIds);
+		if (segmentsEntryIds == null) {
+			segmentsEntryIds = _getSegmentsEntryIds(
+				httpServletRequest, themeDisplay.getScopeGroupId(),
+				themeDisplay.getUserId());
+
+			httpServletRequest.setAttribute(
+				SegmentsWebKeys.SEGMENTS_ENTRY_IDS, segmentsEntryIds);
+		}
 
 		if (!layout.isTypeContent()) {
 			return;
@@ -133,7 +138,8 @@ public class SegmentsServicePreAction extends Action {
 			SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS,
 			_getSegmentsExperienceIds(
 				httpServletRequest, httpServletResponse, layout.getGroupId(),
-				segmentsEntryIds,
+				ArrayUtil.append(
+					segmentsEntryIds, SegmentsEntryConstants.ID_DEFAULT),
 				_portal.getClassNameId(Layout.class.getName()),
 				layout.getPlid()));
 	}
