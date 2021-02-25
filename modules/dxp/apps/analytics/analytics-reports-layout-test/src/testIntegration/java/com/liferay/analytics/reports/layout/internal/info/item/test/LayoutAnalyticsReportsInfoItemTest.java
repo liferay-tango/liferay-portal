f8ddd9c8c1c67d857509e9c29dd61c73b574d9b1
Cristina González Castellano <cristina.gonzalez@liferay.com>
2021-02-25 17:57:40 +0100

    LPS-126044 Test it

--- modules/dxp/apps/analytics/analytics-reports-layout-test/src/testIntegration/java/com/liferay/analytics/reports/layout/internal/info/item/test/LayoutAnalyticsReportsInfoItemTest.java
+++ modules/dxp/apps/analytics/analytics-reports-layout-test/src/testIntegration/java/com/liferay/analytics/reports/layout/internal/info/item/test/LayoutAnalyticsReportsInfoItemTest.java
@@ -16,12 +16,16 @@ package com.liferay.analytics.reports.layout.internal.info.item.test;
 
 import com.liferay.analytics.reports.info.item.AnalyticsReportsInfoItem;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.info.type.WebImage;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -29,9 +33,11 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
@@ -45,6 +51,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Cristina González
@@ -142,6 +150,68 @@ public class LayoutAnalyticsReportsInfoItemTest {
 	}
 
 	@Test
+	public void testGetAuthorWebImage() throws Exception {
+		User user = UserTestUtil.addUser(_group.getGroupId());
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, new ThemeDisplay());
+
+		serviceContext.setRequest(mockHttpServletRequest);
+
+		ServiceContextThreadLocal.pushServiceContext(serviceContext);
+
+		try {
+			Layout layout = _layoutLocalService.addLayout(
+				user.getUserId(), _group.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				StringPool.BLANK, LayoutConstants.TYPE_CONTENT, false,
+				StringPool.BLANK, serviceContext);
+
+			WebImage webImage = _analyticsReportsInfoItem.getAuthorWebImage(
+				layout, LocaleUtil.getSiteDefault());
+
+			String url = webImage.getUrl();
+
+			Assert.assertTrue(url.contains("user_portrait?"));
+		}
+		finally {
+			_userLocalService.deleteUser(user);
+
+			ServiceContextThreadLocal.popServiceContext();
+		}
+	}
+
+	@Test
+	public void testGetAuthorWebImageWithoutThemeDisplay() throws Exception {
+		User user = UserTestUtil.addUser(_group.getGroupId());
+
+		try {
+			Layout layout = _layoutLocalService.addLayout(
+				user.getUserId(), _group.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				StringPool.BLANK, LayoutConstants.TYPE_CONTENT, false,
+				StringPool.BLANK,
+				ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+			WebImage webImage = _analyticsReportsInfoItem.getAuthorWebImage(
+				layout, LocaleUtil.getSiteDefault());
+
+			Assert.assertEquals(StringPool.BLANK, webImage.getUrl());
+		}
+		finally {
+			_userLocalService.deleteUser(user);
+		}
+	}
+
+	@Test
 	public void testGetAvailableLocales() throws Exception {
 		Layout layout = _layoutLocalService.addLayout(
 			TestPropsValues.getUserId(), _group.getGroupId(), false,
@@ -159,6 +229,80 @@ public class LayoutAnalyticsReportsInfoItemTest {
 		Assert.assertEquals(
 			Arrays.asList(LocaleUtil.BRAZIL, LocaleUtil.SPAIN),
 			_analyticsReportsInfoItem.getAvailableLocales(layout));
+	}
+
+	@Test
+	public void testGetCanonicalURL() throws Exception {
+		User user = UserTestUtil.addUser(_group.getGroupId());
+
+		try {
+			ServiceContext serviceContext =
+				ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+			MockHttpServletRequest mockHttpServletRequest =
+				new MockHttpServletRequest();
+
+			mockHttpServletRequest.setAttribute(
+				WebKeys.CURRENT_COMPLETE_URL, StringPool.BLANK);
+
+			ThemeDisplay themeDisplay = new ThemeDisplay();
+
+			themeDisplay.setCompany(
+				_companyLocalService.fetchCompany(
+					TestPropsValues.getCompanyId()));
+
+			Layout layout = _layoutLocalService.addLayout(
+				user.getUserId(), _group.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				StringPool.BLANK, LayoutConstants.TYPE_CONTENT, false,
+				StringPool.BLANK, serviceContext);
+
+			themeDisplay.setLayoutSet(layout.getLayoutSet());
+
+			themeDisplay.setRequest(mockHttpServletRequest);
+			themeDisplay.setSiteGroupId(_group.getGroupId());
+
+			mockHttpServletRequest.setAttribute(
+				WebKeys.THEME_DISPLAY, themeDisplay);
+
+			serviceContext.setRequest(mockHttpServletRequest);
+
+			ServiceContextThreadLocal.pushServiceContext(serviceContext);
+
+			String canonicalURL = _analyticsReportsInfoItem.getCanonicalURL(
+				layout, LocaleUtil.getSiteDefault());
+
+			Assert.assertTrue(canonicalURL.contains(_group.getFriendlyURL()));
+		}
+		finally {
+			_userLocalService.deleteUser(user);
+
+			ServiceContextThreadLocal.popServiceContext();
+		}
+	}
+
+	@Test
+	public void testGetCanonicalURLWithoutThemeDisplay() throws Exception {
+		User user = UserTestUtil.addUser(_group.getGroupId());
+
+		try {
+			Layout layout = _layoutLocalService.addLayout(
+				user.getUserId(), _group.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID,
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				StringPool.BLANK, LayoutConstants.TYPE_CONTENT, false,
+				StringPool.BLANK,
+				ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+			Assert.assertEquals(
+				StringPool.BLANK,
+				_analyticsReportsInfoItem.getCanonicalURL(
+					layout, LocaleUtil.getSiteDefault()));
+		}
+		finally {
+			_userLocalService.deleteUser(user);
+		}
 	}
 
 	@Test
@@ -226,6 +370,9 @@ public class LayoutAnalyticsReportsInfoItemTest {
 
 	@Inject(filter = "component.name=*.LayoutAnalyticsReportsInfoItem")
 	private AnalyticsReportsInfoItem<Layout> _analyticsReportsInfoItem;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
 
 	@DeleteAfterTestRun
 	private Group _group;
