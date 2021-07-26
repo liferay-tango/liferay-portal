af3c2d412ccdd84a97f35e2a9217d0315ca0daba
Yurena Cabrera <yurena.cabrera@liferay.com>
2021-07-26 12:37:14 +0100

    LPS-131187 Added getType method to ContentDashboardItem interface - Added getType method to inteface's implementations

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/JournalArticleContentDashboardItem.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/JournalArticleContentDashboardItem.java
@@ -315,6 +315,11 @@ public class JournalArticleContentDashboardItem
 	}
 
 	@Override
+	public String getType() {
+		return _JOURNAL_ARTICLE_TYPE;
+	}
+
+	@Override
 	public long getUserId() {
 		if (_latestApprovedJournalArticle != null) {
 			return _latestApprovedJournalArticle.getUserId();
@@ -411,6 +416,8 @@ public class JournalArticleContentDashboardItem
 				String.valueOf(curJournalArticle.getVersion()))
 		);
 	}
+
+	private static final String _JOURNAL_ARTICLE_TYPE = "Web Content";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		JournalArticleContentDashboardItem.class);
