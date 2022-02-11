43bbb2c704a566307de41d2e62758385290b920e
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-02-11 07:33:26 +0000

    LPS-132509 We should avoid using base classes in APIs (ContentDashboardBaseItem).
    If we maintain it, all the modules that use this API will need to use -dsannotations-options: inherit, as we only need one method, it is not worth the cost of support this annotation is every possible client.

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/BlogsEntryContentDashboardItem.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/BlogsEntryContentDashboardItem.java
@@ -20,6 +20,7 @@ import com.liferay.blogs.model.BlogsEntry;
 import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
 import com.liferay.content.dashboard.item.action.exception.ContentDashboardItemActionException;
 import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemActionProvider;
+import com.liferay.content.dashboard.web.internal.info.item.provider.util.InfoItemFieldValuesProviderUtil;
 import com.liferay.content.dashboard.web.internal.item.action.ContentDashboardItemActionProviderTracker;
 import com.liferay.content.dashboard.web.internal.item.type.ContentDashboardItemSubtype;
 import com.liferay.content.dashboard.web.internal.util.ContentDashboardGroupUtil;
@@ -58,7 +59,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author Cristina González
  */
 public class BlogsEntryContentDashboardItem
-	extends BaseContentDashboardItem<BlogsEntry> {
+	implements ContentDashboardItem<BlogsEntry> {
 
 	public BlogsEntryContentDashboardItem(
 		List<AssetCategory> assetCategories, List<AssetTag> assetTags,
@@ -263,6 +264,12 @@ public class BlogsEntryContentDashboardItem
 	}
 
 	@Override
+	public String getDescription(Locale locale) {
+		return InfoItemFieldValuesProviderUtil.getStringValue(
+			_blogsEntry, _infoItemFieldValuesProvider, "description");
+	}
+
+	@Override
 	public Object getDisplayFieldValue(String fieldName, Locale locale) {
 		InfoFieldValue<Object> infoFieldValue =
 			_infoItemFieldValuesProvider.getInfoFieldValue(
@@ -273,16 +280,6 @@ public class BlogsEntryContentDashboardItem
 		}
 
 		return infoFieldValue.getValue(locale);
-	}
-
-	@Override
-	public BlogsEntry getInfoItem() {
-		return _blogsEntry;
-	}
-
-	@Override
-	public InfoItemFieldValuesProvider getInfoItemFieldValuesProvider() {
-		return _infoItemFieldValuesProvider;
 	}
 
 	@Override
--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/JournalArticleContentDashboardItem.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/JournalArticleContentDashboardItem.java
@@ -19,6 +19,7 @@ import com.liferay.asset.kernel.model.AssetTag;
 import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
 import com.liferay.content.dashboard.item.action.exception.ContentDashboardItemActionException;
 import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemActionProvider;
+import com.liferay.content.dashboard.web.internal.info.item.provider.util.InfoItemFieldValuesProviderUtil;
 import com.liferay.content.dashboard.web.internal.item.action.ContentDashboardItemActionProviderTracker;
 import com.liferay.content.dashboard.web.internal.item.type.ContentDashboardItemSubtype;
 import com.liferay.content.dashboard.web.internal.util.ContentDashboardGroupUtil;
@@ -57,7 +58,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author Cristina González
  */
 public class JournalArticleContentDashboardItem
-	extends BaseContentDashboardItem<JournalArticle> {
+	implements ContentDashboardItem<JournalArticle> {
 
 	public JournalArticleContentDashboardItem(
 		List<AssetCategory> assetCategories, List<AssetTag> assetTags,
@@ -267,6 +268,12 @@ public class JournalArticleContentDashboardItem
 	}
 
 	@Override
+	public String getDescription(Locale locale) {
+		return InfoItemFieldValuesProviderUtil.getStringValue(
+			_journalArticle, _infoItemFieldValuesProvider, "description");
+	}
+
+	@Override
 	public Object getDisplayFieldValue(String fieldName, Locale locale) {
 		InfoFieldValue<Object> infoFieldValue =
 			_infoItemFieldValuesProvider.getInfoFieldValue(
@@ -277,16 +284,6 @@ public class JournalArticleContentDashboardItem
 		}
 
 		return infoFieldValue.getValue(locale);
-	}
-
-	@Override
-	public JournalArticle getInfoItem() {
-		return _journalArticle;
-	}
-
-	@Override
-	public InfoItemFieldValuesProvider getInfoItemFieldValuesProvider() {
-		return _infoItemFieldValuesProvider;
 	}
 
 	@Override
