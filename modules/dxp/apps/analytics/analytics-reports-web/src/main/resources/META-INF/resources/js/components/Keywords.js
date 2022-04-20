e8543ba33d131518a0f4d7b659fa47ab313aff05
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-04-20 16:50:22 +0200

    LPS-149256 Strings modifications and changes to display list of countries

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/Keywords.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/components/Keywords.js
@@ -11,20 +11,16 @@
 
 import ClayButton from '@clayui/button';
 import ClayDropDown from '@clayui/drop-down';
-import {ClaySelect} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import ClayList from '@clayui/list';
 import PropTypes from 'prop-types';
 import React, {useContext, useMemo, useState} from 'react';
 
 import {StoreStateContext} from '../context/StoreContext';
-import {numberFormat} from '../utils/numberFormat';
-import Hint from './Hint';
 
 const KEYWORD_VALUE_TYPE = [
-	{label: Liferay.Language.get('traffic'), name: 'traffic'},
-	{label: Liferay.Language.get('search-volume'), name: 'volume'},
-	{label: Liferay.Language.get('position'), name: 'position'},
+	{label: Liferay.Language.get('views'), name: 'views'},
+	{label: Liferay.Language.get('views-percentage'), name: 'views-percentage'},
 ];
 
 export default function Keywords({currentPage}) {
@@ -40,37 +36,18 @@ export default function Keywords({currentPage}) {
 		const dataKeys = new Set();
 
 		return currentPage.data.countrySearchKeywords.reduce(
-			(acc, {countryCode, countryName}) => {
+			(acc, {countryCode, countryName, views, viewsP}) => {
 				if (dataKeys.has(countryCode)) {
 					return acc;
 				}
 
 				dataKeys.add(countryCode);
 
-				return acc.concat({countryCode, countryName});
+				return acc.concat({countryCode, countryName, views, viewsP});
 			},
 			[]
 		);
 	}, [currentPage.data.countrySearchKeywords]);
-
-	const [currentCountry, setCurrentCountry] = useState(
-		countries[0].countryCode
-	);
-
-	const keywords = useMemo(() => {
-		const countryKeywords =
-			currentCountry &&
-			currentPage.data.countrySearchKeywords.find((country) => {
-				return country.countryCode === currentCountry;
-			});
-
-		return countryKeywords?.keywords ?? [];
-	}, [currentPage.data.countrySearchKeywords, currentCountry]);
-
-	const handleCountrySelection = (event) => {
-		const country = event.target.value;
-		setCurrentCountry(country);
-	};
 
 	const handleKeywordValueType = (valueTypeName) => {
 		const newKeywordValueType = KEYWORD_VALUE_TYPE.find(
@@ -84,52 +61,15 @@ export default function Keywords({currentPage}) {
 	return (
 		<>
 			<h5 className="mt-3 sheet-subtitle">
-				{Liferay.Language.get('best-keywords')}
+				{Liferay.Language.get('views-by-location')}
 			</h5>
-
-			<div className="mb-3 text-secondary">
-				{Liferay.Language.get('best-keywords-description')}
-			</div>
-
-			<div className="form-group">
-				<label htmlFor="countrySelect">
-					{Liferay.Language.get('select-country')}
-				</label>
-
-				<ClaySelect
-					aria-label={Liferay.Language.get('select-country')}
-					className="bg-white"
-					defaultValue={currentCountry}
-					id="countrySelect"
-					onChange={handleCountrySelection}
-				>
-					{countries.map((item) => (
-						<ClaySelect.Option
-							key={item.countryCode}
-							label={item.countryName}
-							value={item.countryCode}
-						/>
-					))}
-				</ClaySelect>
-			</div>
 
 			<ClayList className="list-group-keywords-list">
 				<ClayList.Item flex>
 					<ClayList.ItemField expand>
 						<ClayList.ItemTitle className="text-truncate-inline">
 							<span className="text-truncate">
-								{Liferay.Language.get('best-keyword')}
-
-								<span className="text-secondary">
-									<Hint
-										message={Liferay.Language.get(
-											'best-keyword-help'
-										)}
-										title={Liferay.Language.get(
-											'best-keyword'
-										)}
-									/>
-								</span>
+								{Liferay.Language.get('Country')}
 							</span>
 						</ClayList.ItemTitle>
 					</ClayList.ItemField>
@@ -178,19 +118,19 @@ export default function Keywords({currentPage}) {
 				</ClayList.Item>
 
 				{!publishedToday &&
-					keywords.map(
-						({keyword, position, searchVolume, traffic}) => {
+					countries.map(
+						({countryCode, countryName, views, viewsP}) => {
 							return (
-								<ClayList.Item flex key={keyword}>
+								<ClayList.Item flex key={countryCode}>
 									<ClayList.ItemField expand>
 										<ClayList.ItemText>
 											<span
 												className="text-truncate-inline"
 												data-tooltip-align="top"
-												title={keyword}
+												title={countryName}
 											>
 												<span className="text-secondary text-truncate">
-													{keyword}
+													{countryName}
 												</span>
 											</span>
 										</ClayList.ItemText>
@@ -198,36 +138,21 @@ export default function Keywords({currentPage}) {
 
 									<ClayList.ItemField expand>
 										<span className="align-self-end font-weight-semi-bold text-dark">
-											{numberFormat(
-												languageTag,
+											{
 												keywordValueType.name ===
-													'traffic'
-													? traffic
+													'views'
+													? views
 													: keywordValueType.name ===
-													  'volume'
-													? searchVolume
-													: position
-											)}
+													  'views-percentage'
+													? viewsP + '%'
+													: views
+											}
 										</span>
 									</ClayList.ItemField>
 								</ClayList.Item>
 							);
 						}
 					)}
-
-				{(publishedToday || keywords.length === 0) && (
-					<ClayList.Item flex>
-						<ClayList.ItemField expand>
-							<ClayList.ItemText>
-								<span className="text-secondary">
-									{Liferay.Language.get(
-										'there-are-no-best-keywords-yet'
-									)}
-								</span>
-							</ClayList.ItemText>
-						</ClayList.ItemField>
-					</ClayList.Item>
-				)}
 			</ClayList>
 		</>
 	);
