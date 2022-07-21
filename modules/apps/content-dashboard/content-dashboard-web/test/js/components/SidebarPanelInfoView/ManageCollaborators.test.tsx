7b4c176902962733498b531db51d98429f9ddc09
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-07-21 21:35:35 +0200

    LPS-153687 Fix test onError

--- modules/apps/content-dashboard/content-dashboard-web/test/js/components/SidebarPanelInfoView/ManageCollaborators.test.tsx
+++ modules/apps/content-dashboard/content-dashboard-web/test/js/components/SidebarPanelInfoView/ManageCollaborators.test.tsx
@@ -58,4 +58,17 @@ describe('Manage collaborators component', () => {
 			expect(runScriptsInElement).toHaveBeenCalled();
 		});
 	});
+
+	it('handles the API error', async () => {
+		(fetch as jest.Mock).mockImplementation(() => {
+			return {
+				ok: false,
+				text: null,
+			};
+		});
+
+		const {queryByText} = render(_getComponent());
+
+		expect(queryByText('Manage Collaborators')).not.toBeInTheDocument();
+	});
 });
