e0c855115ac8673338873fb31baa2d19195386b0
Mikel Lorza <mikel.lorza@liferay.com>
2023-01-12 12:05:23 +0100

    LPS-171060 Add implementation of if the segments experience is a single modification.

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -844,7 +844,7 @@ public class ContentPageEditorDisplayContext {
 	}
 
 	public boolean isSingleSegmentsExperienceMode() {
-		return false;
+		return _isSegmentsExperimentVariant();
 	}
 
 	public boolean isWorkflowEnabled() {
