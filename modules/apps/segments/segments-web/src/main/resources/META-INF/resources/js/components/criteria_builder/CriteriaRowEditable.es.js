ceccb8312f8d34e8fe2f2b8054615ec8a3b35979
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2023-02-24 16:38:54 +0100

    LPS-176077 Use EditEvent component from the CriteriaRowEditable

--- modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_builder/CriteriaRowEditable.es.js
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_builder/CriteriaRowEditable.es.js
@@ -35,6 +35,7 @@ import EventInput from '../inputs/EventInput';
 import IntegerInput from '../inputs/IntegerInput.es';
 import SelectEntityInput from '../inputs/SelectEntityInput.es';
 import StringInput from '../inputs/StringInput.es';
+import EditEvent from './EditEvent';
 
 class CriteriaRowEditable extends Component {
 	static propTypes = {
@@ -104,6 +105,28 @@ class CriteriaRowEditable extends Component {
 				...value,
 			});
 		}
+	};
+
+	_renderEditableEventProperty = ({
+		criterion,
+		error,
+		propertyLabel,
+		renderEmptyValuesErrors,
+		selectedProperty,
+		value,
+	}) => {
+		return (
+			<EditEvent
+				criterion={criterion}
+				error={error}
+				onChange={this._handleTypedInputChange}
+				onInputChange={this._handleInputChange}
+				propertyLabel={propertyLabel}
+				renderEmptyValuesErrors={renderEmptyValuesErrors}
+				selectedProperty={selectedProperty}
+				value={value}
+			></EditEvent>
+		);
 	};
 
 	_renderEditableProperty = ({
